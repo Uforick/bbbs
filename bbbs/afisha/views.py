012076b8b8480f3804ecd690896d4993443b7ba9
Alexander Gryaznov <ne4istii@gmail.com>
2021-05-27 15:49:40 +0300

    fix eventlist

--- bbbs/afisha/views.py
+++ bbbs/afisha/views.py
@@ -15,10 +15,10 @@ class EventList(generics.ListAPIView):
         if self.request.user.is_superuser:
             events = Event.objects.all()
         elif self.request.user.is_authenticated:
-            self_profile = generics.get_object_or_404(Profile, user=self.request.user)
-            events = Event.objects.filter(city__in=self_profile.get_city)
-        elif self.request.user.is_anonymous:
-            events = Event.objects.filter(city=self.kwargs.get('city'))
+            self_profile = generics.get_object_or_404(Profile, user = self.request.user)
+            events = Event.objects.filter(city__in = self_profile.get_city)
+        elif self.request.user.is_authenticated is False:
+            events = Event.objects.filter(city=self.request.data.get('city'))
         return events
 
 
