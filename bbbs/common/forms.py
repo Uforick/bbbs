ecd3cb6b9640e054719c96be01b9fc4543176cb2
Roman <roman89320@gmail.com>
2021-05-30 07:31:00 +0300

    Some small corrections..

--- bbbs/common/forms.py
+++ bbbs/common/forms.py
@@ -1,12 +1,13 @@
 from django import forms
 
+from bbbs.common.models import Profile
+
 
 class ProfileAdminForm(forms.ModelForm):
     def clean(self):
         city = self.cleaned_data['city']
         role = self.cleaned_data['role']
-        if city.count() > 1 and role == 'MENTOR':
+        if city.count() > 1 and role == Profile.PermissionChoice.MENTOR:
             raise forms.ValidationError(
                 "У наставника не может быть больше одного города."
             )
-
