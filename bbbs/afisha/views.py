fa60b992ba5df5f4c7ca87111470317a18e4f772
Alexander Gryaznov <ne4istii@gmail.com>
2021-05-31 16:43:01 +0300

    refactor(bug fix)

--- bbbs/afisha/views.py
+++ bbbs/afisha/views.py
@@ -15,10 +15,13 @@ class EventList(generics.ListAPIView):
         if self.request.user.is_superuser:
             events = Event.objects.all()
         elif self.request.user.is_authenticated:
-            self_profile = generics.get_object_or_404(Profile, user=self.request.user)
-            events = Event.objects.filter(city__in=self_profile.user_cities)
+            self_profile = generics.get_object_or_404(
+                Profile, user=self.request.user)
+            events = Event.objects.filter(
+                city__in=self_profile.user_cities)
         elif not self.request.user.is_authenticated:
-            events = Event.objects.filter(city=self.request.GET.get('city'))
+            events = Event.objects.filter(
+                city=self.request.query_params.get('city'))
         return events
 
 
