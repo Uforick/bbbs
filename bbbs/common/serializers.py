c2c9f394ee8258a3e5a22cc3e001d980fc36b6c4
Alexander Gryaznov <ne4istii@gmail.com>
2021-06-01 13:32:23 +0300

    refactor(add to_repr ProfileSerializer)

--- bbbs/common/serializers.py
+++ bbbs/common/serializers.py
@@ -1,12 +1,15 @@
+from typing import OrderedDict
 from django.shortcuts import get_object_or_404
 
 from rest_framework import serializers
+from rest_framework import fields
 from rest_framework.fields import empty
 
 from bbbs.common.models import City, Profile
 
 
 class CitySerializer(serializers.ModelSerializer):
+
     class Meta:
         model = City
         fields = serializers.ALL_FIELDS
@@ -16,7 +19,14 @@ class ProfileSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Profile
-        fields = serializers.ALL_FIELDS
+        fields = ('id', 'user', 'city')
+
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+        if instance.is_mentor:
+            data['city'] = data.get('city')[0]
+            return data
+        return data
 
     def validate(self, attrs):
         profile = get_object_or_404(Profile, user=self.context['request'].user)
