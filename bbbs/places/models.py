c6b5e2fe42848196e698bce98f93237271de39b7
Roman <roman89320@gmail.com>
2021-06-15 22:10:14 +0300

    main(not all), place filter (#13)
    * ProfileView (get, put) for current user
    
    * Model refactorind. And added city name validator.
    
    * User can have many cities
    
    * Added validation for  MENTOR (can have only one city)
    
    * Added count city validation for MENTOR in django.admin
    
    * Changed validation for ProfileAdminForm, cleaned settings.py comments etc
    
    * Places (admin.py), PlaceList(/places/)
    
    * Changed Place List(not last version)
    
    * Added Tag to places
    
    * Place method post - working, update -  not working
    
    * Update views.py
    
    * api/v1/place (GET, POST) tag filters for places
    
    * Small changes
    
    * exclude verified field
    
    * main(not all), place filter

--- bbbs/places/models.py
+++ bbbs/places/models.py
@@ -27,6 +27,11 @@ class Place(models.Model):
         ENTERTAINING = 1, gettext_lazy("Развлекательный")
         INFORMATIVE = 2, gettext_lazy("Познавательный")
 
+    verified = models.BooleanField(
+        default=False,
+        verbose_name='Показать на главной',
+        help_text='Установить флаг, если проверено для публикации',
+    )
     chosen = models.BooleanField(
         verbose_name="Выбор наставника",
         default=False,
@@ -78,6 +83,7 @@ class Place(models.Model):
     verified = models.BooleanField(default=False)
 
     class Meta:
+        ordering = ('title',)
         verbose_name = "Место - куда пойти?"
         verbose_name_plural = "Места - куда пойти?"
 
@@ -89,3 +95,6 @@ class Place(models.Model):
 
     def get_activity_type(self, type_code):
         return self.ActivityTypes(type_code).label
+
+    def list_tags(self):
+        return self.tag.values_list('name', flat=True)
--- bbbs/questions/models.py
+++ bbbs/questions/models.py
@@ -1,19 +1,66 @@
 from django.db import models
 
-from bbbs.common.models import Tag
+from pytils.translit import slugify
+
+
+class Tag(models.Model):
+    name = models.CharField(
+        verbose_name='Имя',
+        help_text='Введите имя тега',
+        max_length=50,
+        unique=True
+    )
+    slug = models.SlugField(
+        verbose_name='Ссылка',
+        help_text='Заполняется автоматически',
+        auto_created=True,
+        blank=True,
+        null=True
+    )
+
+    class Meta:
+        verbose_name = 'Тег'
+        verbose_name_plural = 'Теги'
+        ordering = ('-name',)
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
 
 
 class Question(models.Model):
-    tag = models.ManyToManyField(Tag, related_name='tags')
-    question = models.CharField(max_length=500, unique=True)
-    answer = models.TextField(verbose_name='Ответ на вопрос')
+    verified = models.BooleanField(
+        default=False,
+        verbose_name='Показать на главной',
+        help_text='Установить флаг, если проверено для публикации',
+    )
+    tag = models.ManyToManyField(
+        Tag,
+        verbose_name='Тег',
+        help_text='Выберите один или несколько тегов',
+        related_name='tags',
+        blank=True
+    )
+    question = models.CharField(
+        max_length=500,
+        unique=True,
+        verbose_name='Вопрос'
+    )
+    answer = models.TextField(
+        verbose_name='Ответ на вопрос',
+        blank=True,
+        null=True
+    )
 
     class Meta:
         verbose_name = 'Вопрос'
         verbose_name_plural = 'Вопросы'
 
     def list_tags(self):
-        return self.tag.values_list('name')
+        return self.tag.values_list('name', flat=True)
 
     def __str__(self):
         return self.question
