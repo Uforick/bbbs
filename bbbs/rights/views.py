7c4fcb516fd575df83dfa80c9962d8e1611c0c6b
Alexander Gryaznov <ne4istii@gmail.com>
2021-07-05 23:30:58 +0300

    fix Rights perms

--- bbbs/rights/views.py
+++ bbbs/rights/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import get_object_or_404
-from rest_framework import filters, generics, permissions
+from rest_framework import filters, generics
 
 from bbbs.rights.models import Right, RightTag
 from bbbs.rights.serializers import RightSerializer, RightTagSerializer
@@ -8,14 +8,12 @@ from bbbs.rights.serializers import RightSerializer, RightTagSerializer
 class RightList(generics.ListAPIView):
     queryset = Right.objects.all()
     serializer_class = RightSerializer
-    permission_classes = [permissions.IsAuthenticated]
     filter_backends = [filters.SearchFilter]
     search_fields = ['=tag__name']
 
 
 class RightView(generics.RetrieveAPIView):
     serializer_class = RightSerializer
-    permission_classes = [permissions.IsAuthenticated]
     pagination_class = None
 
     def get_object(self):
@@ -26,6 +24,5 @@ class RightView(generics.RetrieveAPIView):
 class RightTagList(generics.ListAPIView):
     queryset = RightTag.objects.all()
     serializer_class = RightTagSerializer
-    permission_classes = [permissions.IsAuthenticated]
     filterset_fields = ['slug']
     pagination_class = None
